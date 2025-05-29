4a057c8d5eb49710dbfab3f19ad9b4a15f9f07e4
ProfeOscarUni <odbautista@profesores.uniajc.edu.co>
2025-05-29 18:54:41 -0500

    1234

--- test_main.py
+++ test_main.py
@@ -27,11 +27,3 @@ def test_sum3():
 def test_sum_param(inX,inY, esperado):
     assert sum(inX, inY)==esperado
     print("La función suma parametrizada funciona que trabaja correctamente")
-    
-def test_login_pass():
-    login_passes = login("Pruebas y herramientas", "123456")
-    assert login_passes
-    
-def test_login_fail():
-    login_fails = login("Prueba y Herramienta", "1234567")
-    assert not login_fails
